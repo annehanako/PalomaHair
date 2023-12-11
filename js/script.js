0d7a4dbf8b7c47377ff5711594079f75ab354af5
annehanako <anne.f.hanako@gmail.com>
2023-12-11 17:47:24 -0300

    final version on work

--- js/script.js
+++ js/script.js
@@ -8,3 +8,11 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+document.addEventListener('DOMContentLoaded', function () {
+    const toggleBtn = document.querySelector('.toggle-btn');
+    const sidebar = document.querySelector('.sidebar');
+
+    toggleBtn.addEventListener('click', function () {
+        sidebar.style.width = sidebar.style.width === '250px' ? '0' : '250px';
+    });
+});
